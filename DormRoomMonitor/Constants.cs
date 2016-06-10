ceb73bd1870b2573f02f6ecfb0f04cfdfb448d9e
Prasantha Jayakody <prasanthajayakody@hotmail.com>
2016-06-10 11:31:02 -0700

    updated signup url

--- DormRoomMonitor/Constants.cs
+++ DormRoomMonitor/Constants.cs
@@ -10,7 +10,7 @@
         public const bool DisableLiveCameraFeed = true;
 
         // Oxford Face API Primary should be entered here
-        // You can obtain a subscription key for Face API by following the instructions here: https://www.projectoxford.ai/doc/general/subscription-key-mgmt
+        // You can obtain a subscription key for Face API by following the instructions here: https://www.microsoft.com/cognitive-services/en-us/sign-up
         public const string OxfordAPIKey = "b2b1adb22ce44bcc91e5cdfe2afb4f53";
 
         // Name of the folder in which all Whitelist data is stored
